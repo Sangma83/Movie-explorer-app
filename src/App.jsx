d3772bfdb3932380518092a5ea35d9d1f320a030
Sangma83 <poushalisangma13@gmail.com>
2026-09-17 11:58:50 +0600

    Hero section started with nav and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home/>,
   },
 ]);
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <h2>This is Home page</h2>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
